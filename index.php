0d1d3a227b64e5434994e8b61d031c8a9ba08198
mickaelrutkowski <mickael.rutkowski@gmail.com>
2018-11-27 15:56:35 +0100

    exercice_php_p5E7

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+$dep = array( 80 =>'Somme', 60 =>'Oise', 62 =>'Pas de Calais', 02 =>'Aisne', 59 =>'Nord');
+$dep = array(51 => 'Reims');
+echo 'Vous vous trouvez dans le départempent' . ' ' .$dep[51] = 'Marne';
+?>
